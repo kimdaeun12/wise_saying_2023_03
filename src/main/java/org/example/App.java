d6b705fd14f188beeaca07c3ff0fbdc54e925be3
kimdaeun12 <1004ekdms2@naver.com>
2023-03-27 13:46:30 +0900

    1단계 완료

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,2 +1,21 @@
-package org.example;public class App {
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    private Scanner sc;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void run() {
+        System.out.println("==명언 앱 ==");
+        while (true) {
+            String command = sc.nextLine().trim();
+
+            if (command.equals("종료"))
+                break;
+        }
+    }
 }
